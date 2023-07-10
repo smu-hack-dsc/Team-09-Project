d38e2579b6f279d9a5c33ee61d0b4f3655691354
kai xuan <tankaixuan24@gmail.com>
2023-07-10 18:21:27 +0800

    Google auth running on backend.Not linked to front

--- Backend/server.js
+++ Backend/server.js
@@ -4,14 +4,60 @@
 //     console.log("App hosted at localhost:" + port);
 // });
 
-const app = require('express')();
-const PORT = 3000;
+const express = require('express');
+const app = express();
 
-app.listen(
-    PORT,() => console.log(`It's alive on http://localhost:${PORT}`)
-);
+const passport = require('passport');
+const PORT = 3000; //change to .env variable
+
+const session = require('express-session');
+app.use(session({
+    secret:'cats', // change to .env variable
+    resave: false,
+    saveUninitialized: false,
+})); 
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./auth');
  
-// app.listen(PORT, function(err){
-//     if (err) console.log("Error in server setup")
-//     console.log("Server listening on Port", PORT);
-// })
+function isLoggedIn(req,res,next) {
+    req.user ? next() : res.sendStatus(401);
+};
+
+app.get('/',(req,res) => {
+    res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+app.get('/auth/google',
+    passport.authenticate('google',{scope:['email','profile']})
+);    
+
+app.get('/google/callback',
+    passport.authenticate('google',{
+        successRedirect: '/protected',
+        failureRedirect: '/auth/failure',
+    }));
+
+app.get('/auth/failure', (req,res) => {
+    res.send('Authentication failed');
+});
+
+// adding isLoggedIn middleware function is called before the res is sent.
+app.get('/protected',isLoggedIn, (req,res) => {
+    console.log(req.user);
+    res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get('/logout', (req,res) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        req.session.destroy();
+        res.redirect('/');
+      });
+});
+
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+});
