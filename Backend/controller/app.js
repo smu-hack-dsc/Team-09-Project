e8aaaf28d978defe8c28eeef95b88f8b3f7b847c
netteee <lynettetay48@gmail.com>
2023-07-11 12:01:06 +0800

    jwt install

--- Backend/controller/app.js
+++ Backend/controller/app.js
@@ -1,11 +1,12 @@
-// const express = require('express');
+const express = require('express');
+const verifyToken = require('../auth/verifyToken')
 
-// const app = express();
+const app = express();
 
-// const bodyParser = require('body-parser'); 
-// const urlencodedParser = bodyParser.urlencoded({ 
-//     extended:false 
-// }); 
+const bodyParser = require('body-parser'); 
+const urlencodedParser = bodyParser.urlencoded({ 
+    extended:false 
+}); 
 
-// app.use(bodyParser.json);
-// app.use(urlencodedParser);
+app.use(bodyParser.json);
+app.use(urlencodedParser);
