2bda7a3ca19d99567627feecfb84445cb52dc47d
theodicyy <50317261+facelessman2000@users.noreply.github.com>
2023-07-26 16:40:51 +0800

    responsiveness

--- Backend/model/databaseconfig.js
+++ Backend/model/databaseconfig.js
@@ -3,9 +3,10 @@ const mysql = require('mysql');
 // Create a connection pool
 const pool = mysql.createPool({
     host:"localhost",
+    port:8889,
     user:"root",
     // CHANGE PASSWORD TO root IF USING MAC or LEAVE IT EMPTY IN WINDOWS
-    password:"",
+    password:"root",
     database:"HEAP"
 });
 
