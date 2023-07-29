bcea7d339246dceed04965958c376331c375f72b
netteee <lynettetay48@gmail.com>
2023-07-30 01:44:14 +0800

    cleaning up

--- Backend/auth.js
+++ Backend/auth.js
@@ -2,9 +2,6 @@ require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-
-
-
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
