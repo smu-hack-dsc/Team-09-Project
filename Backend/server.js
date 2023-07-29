1c9763fe2b9997e752cc0e68ae23e85880c217b0
netteee <lynettetay48@gmail.com>
2023-07-30 01:51:28 +0800

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -234,4 +233,3 @@ app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", PORT);
 })
-
