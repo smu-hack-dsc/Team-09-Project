31c14410c283021d47540ac67608809311b5f456
theodicyy <50317261+facelessman2000@users.noreply.github.com>
2023-07-29 22:14:51 +0800

    modified:   Backend/model/databaseconfig.js 	modified:   Frontend/public/available.html 	modified:   Frontend/public/css/style.css 	new file:   Frontend/public/js/navbar.js

---
+++ Frontend/public/js/navbar.js
@@ -0,0 +1,30 @@
+/*const hamburger = document.getElementById('hamburger')
+const navLinks = document.getElementById('nav_links');
+const closePopup = document.getElementById('close_popup');
+hamburger.addEventListener('click', e => {
+  console.log('tet')
+  nav_links.classList.add('show')
+  hamburger.classList.add('hide')
+})
+
+closePopup.addEventListener('click', () => {
+    nav_links.classList.remove('show');
+    hamburger.classList.remove('hide')
+
+})
+*/
+
+const hamburger = document.getElementById('hamburger_popup');
+const navLinks = document.getElementById('nav_links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+// Add this event listener to hide the nav links when any of the links is clicked
+const navLinksItems = document.querySelectorAll('.nav_links li a');
+navLinksItems.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
+});
