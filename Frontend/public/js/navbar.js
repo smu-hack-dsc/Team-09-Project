fabfaac03d3d09ffbdafa7748dd6030bb8e56e49
theodicyy <50317261+facelessman2000@users.noreply.github.com>
2023-07-30 15:38:20 +0800

    some updates

--- Frontend/public/js/navbar.js
+++ Frontend/public/js/navbar.js
@@ -1,4 +1,4 @@
-const hamburger = document.getElementById('hamburger')
+/*const hamburger = document.getElementById('hamburger')
 const navLinks = document.getElementById('nav_links');
 const closePopup = document.getElementById('close_popup');
 hamburger.addEventListener('click', e => {
@@ -11,7 +11,23 @@ closePopup.addEventListener('click', () => {
     nav_links.classList.remove('show');
     hamburger.classList.remove('hide')
 
-})
+})*/
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav_links');
+const closePopupLink = document.querySelector('.close_popup a');
+
+hamburger.addEventListener('click', () => {
+  console.log('test');
+  navLinks.classList.add('show');
+  hamburger.classList.add('hide');
+});
+
+closePopupLink.addEventListener('click', (e) => {
+  e.preventDefault(); // Prevent the default behavior of the link (e.g., navigating to the href)
+  navLinks.classList.remove('show');
+  hamburger.classList.remove('hide');
+});
 
 // const hamburger = document.getElementById('hamburger_popup');
 // const navLinks = document.getElementById('nav_links');
