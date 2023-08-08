7baaeba598d7988ae17ddce4e762fef63d51daf8
Paul <pbmadhavan.2022@scis.smu.edu.sg>
2023-08-08 11:38:18 +0800

    clean code

--- Backend/server.js
+++ Backend/server.js
@@ -331,7 +331,9 @@ app.get('/username', async (req,res) => {
 
 app.get('/logout', (req,res) => {
     req.logout(function(err) {
-        if (err) { return next(err); }
+        if (err) { 
+          return next(err); 
+        }
         req.session.destroy();
         res.redirect('http://localhost:3001');
       });
