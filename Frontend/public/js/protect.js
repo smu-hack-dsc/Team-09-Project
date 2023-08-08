f1e89a165e8873e9bf72deb7c4e5b6d1246e62c3
Paul <pbmadhavan.2022@scis.smu.edu.sg>
2023-08-08 12:27:58 +0800

    auth issues

--- Frontend/public/js/protect.js
+++ Frontend/public/js/protect.js
@@ -15,14 +15,27 @@ function removeAccessToken() {
 
 // Main Functions ------------------------
 function isLoggedIn() {
-    const accessToken = sessionStorage.getItem('accessToken');
-    if (accessToken) {
-        console.log('Logged in with token:', accessToken);
-    }
-     else {
-        window.location.href = 'http://localhost:3001';
-     }
+    axios
+        .get('http://localhost:3000/access', { withCredentials: true })
+        .then(res => {
+            const accessToken = res.data;
+            if (accessToken) {
+                console.log('Logged in with token:', accessToken);
+            } else {
+                window.location.href = 'http://localhost:3001';
+            }
+        })
+        .catch(error => {
+            console.error('Error checking access token:', error);
+            window.location.href = 'http://localhost:3001'; // Redirect on error
+        });
 }
+
+function logout() {
+    removeAccessToken();
+    window.location.href = 'http://localhost:3001';
+}
+
 
 // General Script ------------------------
 const accessToken = getAccessToken();
